335cf54d1c1294b3d9048727308636e1309a14fd
Jhonatan Nobrega <jhonatan_nobrega@hotmail.com>
2022-11-10 22:54:09 -0300

    Aula 1

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from './src/components/Loading';
+import { SingIn } from './src/screens/SingIn';
+import { THEME } from './src/styles/theme';
+
+export default function App() {
+  const [fontsLoader] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoader ? <SingIn /> : <Loading />}
+    </NativeBaseProvider>
+  );
+}
